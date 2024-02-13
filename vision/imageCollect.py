26ea2bcc10ecefecfd748293bfebac2a58fbadc4
Ye-2077 <12111813@mail.sustech.edu.cn>
2024-02-13 15:21:03 -0800

    sample2

--- vision/imageCollect.py
+++ vision/imageCollect.py
@@ -3,12 +3,12 @@ import os
 import time
 import cv2
 
-IMAGE_PATH = os.path.join('data', 'images/sample1')
+IMAGE_PATH = os.path.join('vision/data', 'images/sample2')
 # labels = ['puck', 'player1', 'player2', 'table']
 num_imgs = 50
 
 ## collect images
-cap = cv2.VideoCapture("../vision/samples/sample1.mp4")
+cap = cv2.VideoCapture("vision/samples/sample2.mp4")
 
 # for label in labels:
 for img_num in range(num_imgs):
@@ -17,8 +17,8 @@ for img_num in range(num_imgs):
     ret, frame = cap.read()
     img_name = os.path.join(IMAGE_PATH, str(uuid.uuid1())+'.jpg')
     cv2.imwrite(img_name, frame)
-    cv2.imshow('frame', frame)
-    time.sleep(2)
+    # cv2.imshow('frame', frame)
+    time.sleep(3)
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
