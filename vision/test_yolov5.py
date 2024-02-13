08842e622fc7e093608ee5c496b05621da88f9d4
Ye-2077 <2390227785@qq.com>
2024-02-13 09:01:52 +0800

    test yolo add sample video

---
+++ vision/test_yolov5.py
@@ -0,0 +1,21 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5m', pretrained=True)
+
+cap = cv2.VideoCapture('vision/samples/sample3.mp4')
+cv2.namedWindow('YOLO', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('YOLO', 800, 600)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    results = model(frame)
+
+    cv2.imshow('YOLO', np.squeeze(results.render()))
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
