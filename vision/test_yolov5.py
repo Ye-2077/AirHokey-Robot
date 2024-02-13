397056a73ead54e4cc6e4313c170418f03a33b8b
Ye-2077 <2390227785@qq.com>
2024-02-14 07:06:33 +0800

    trained model

--- vision/test_yolov5.py
+++ vision/test_yolov5.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5m', pretrained=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom' , path = 'vision/yolov5-master/runs/train/yolo_puck_1/weights/best.pt', force_reload=True).to('cuda')
 
-cap = cv2.VideoCapture('vision/samples/sample3.mp4')
+cap = cv2.VideoCapture('vision/samples/sample1.mp4')
 cv2.namedWindow('YOLO', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('YOLO', 800, 600)
 
