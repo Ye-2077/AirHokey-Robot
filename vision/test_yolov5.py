97c5089d6e564f57b80d9f26d430eec6b318bad2
Ye-2077 <12111813@mail.sustech.edu.cn>
2024-03-18 10:39:48 -0700

    modified:   vision/test.ipynb 	modified:   vision/test_yolov5.py

--- vision/test_yolov5.py
+++ vision/test_yolov5.py
@@ -5,8 +5,8 @@ import cv2
 
 model = torch.hub.load('ultralytics/yolov5', 'custom' , path = 'vision/yolov5-master/runs/train/yolo_puck_1/weights/best.pt', force_reload=True).to('cuda')
 
-# cap = cv2.VideoCapture('vision/samples/sample2.mp4')
-cap = cv2.VideoCapture(-1)
+cap = cv2.VideoCapture('vision/samples/sample2.mp4')
+# cap = cv2.VideoCapture(-1)
 cv2.namedWindow('YOLO', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('YOLO', 800, 600)
 
