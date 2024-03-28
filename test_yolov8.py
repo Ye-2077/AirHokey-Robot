8587ad99033652e1cad5dbebda5296bc49d5f7dd
Ye-2077 <2390227785@qq.com>
2024-03-28 15:14:14 -0700

    combine pos_detector and test_yolov5 into detector

--- test_yolov8.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-# Configure the tracking parameters and run the tracker
-model = YOLO('vision/yolo_models/yolov8/yolov8n.pt').to('cuda')
-results = model.track(source="vision/samples/sample1.mp4", conf=0.1, iou=0.5, show=True)
